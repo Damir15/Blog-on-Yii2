5f0d3c364da588015d458ba9b790089c50d97c23
Damir15 <krosava_15d@mail.ru>
2017-12-05 01:02:53 +0600

    add file

---
+++ models/User.php
@@ -0,0 +1,110 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+use yii\web\IdentityInterface;
+
+/**
+ * This is the model class for table "user".
+ *
+ * @property integer $id
+ * @property string $name
+ * @property string $email
+ * @property string $password
+ * @property integer $isAdmin
+ * @property string $photo
+ *
+ * @property Comment[] $comments
+ */
+class User extends \yii\db\ActiveRecord implements IdentityInterface
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'user';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['name', 'password'], 'required'],
+            [['name', 'email', 'password', 'photo'], 'string'],
+            [['isAdmin'], 'integer'],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'name' => 'Name',
+            'email' => 'Email',
+            'password' => 'Password',
+            'isAdmin' => 'Is Admin',
+            'photo' => 'Photo',
+        ];
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getComments()
+    {
+        return $this->hasMany(Comment::className(), ['user_id' => 'id']);
+    }
+
+    public static function findIdentity($id)
+    {
+        return static::findOne($id);
+    }
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public static function findIdentityByAccessToken($token, $type = null)
+    {
+        // return static::findOne(['access_token' => $token]);
+    }
+
+    public function getAuthKey()
+    {
+        // return $this->authKey;
+    }
+
+    public function validateAuthKey($authKey)
+    {
+        // return $this->authKey === $authKey;
+    }
+
+    public static function findByEmail($email)
+    {
+        return self::find()->where(['email' => $email])->one();
+    }
+
+    public function validatePassword($password)
+    {
+        return ($this->password == $password) ? true : false;
+    }
+
+    public function create()
+    {
+        return $this->save(false);
+    }
+
+    public function getImage()
+    {
+        return $this->photo ? '/web/uploads/' . $this->photo : '/web/uploads/default.png'; 
+    }
+
+}
